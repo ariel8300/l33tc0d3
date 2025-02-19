fe53aeb6f8806e61b737f619f031651d7ed690c7
Ariel Yosupov <ariel8300@gmail.com>
2025-02-19 19:54:16 +0200

    feat: solved 48

--- arrays/48. Rotate Image.py
+++ arrays/48. Rotate Image.py
@@ -16,9 +16,24 @@
 # -1000 <= matrix[i][j] <= 1000
 
 
+# Solution: Transpose and then reverse
 class Solution:
     def rotate(self, matrix: list[list[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for r in range(len(matrix)):
+            matrix[r].reverse()
+        print(*matrix, sep="\n")
+
+
+Solution().rotate([
+    [5, 1, 9, 11], 
+    [2, 4, 8, 10], 
+    [13, 3, 6, 7], 
+    [15, 14, 12, 16]
+])
